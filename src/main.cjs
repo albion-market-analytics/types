fa4e4036e2bb43ae699b067d6936290c6604834b
Max Zinn <max.zinn8996@gmail.com>
2024-03-29 14:33:55 +0100

    feat: Add index.ts for constants

--- src/main.cjs
+++ src/main.cjs
@@ -8,11 +8,18 @@ const outPath = path.resolve(__dirname, '../index.d.ts');
 (async () => {
     const worldResponse = await axios.get('https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/world.json');
     const itemsResponse = await axios.get('https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json');
-    ejs.renderFile(path.resolve(__dirname, './template.ejs'), {
+    ejs.renderFile(path.resolve(__dirname, './index.d.ts.ejs'), {
         world: worldResponse.data,
         items: itemsResponse.data
     }, {}, (err, str) => {
         fs.writeFileSync(outPath, str, 'utf8');
+    });
+
+    ejs.renderFile(path.resolve(__dirname, './index.ts.ejs'), {
+        world: worldResponse.data,
+        items: itemsResponse.data
+    }, {}, (err, str) => {
+        fs.writeFileSync(path.resolve(__dirname, '../index.ts'), str, 'utf8');
     });
 })();
 
